a5e846fc610c131d5aefd59acb53d147f167efd2
Gbohunmi <gbohunmi.odutuga@gmail.com>
2024-12-21 21:35:44 +0100

    Customized Reminder Variable

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,25 +1,31 @@
-task = input ( "Enter your task:" )
+task = input ( "Enter your task:", )
 priority = input ( "Priority (high/medium/low):" )
 time_bound = input ( "Is it time-bound? (yes/no):" )
 
 match priority:
     case "high":
         if time_bound == "yes":
-            print(task, "is a high priority task that requires immediate attention today!")
+            reminder = (task + " is a high priority task that requires immediate attention today!")
+            print(reminder)
         else:
-            print(task, "is a high priority task. Consider completing it when you have free time.")
+            reminder = (task + " is a high priority task. Consider completing it when you have free time.")
+            print(reminder)
 
     case "medium":
         if time_bound == "yes":
-            print(task, "is a medium priority task that requires immediate attention today!")
+            reminder = (task + " is a medium priority task that requires immediate attention today!")
+            print(reminder)
         else:
-            print(task, "is a medium priority task. Consider completing it when you have free time.")
+            reminder = (task + " is a medium priority task. Consider completing it when you have free time.")
+            print(reminder)
 
     case "low":
         if time_bound == "yes":
-            print(task, "is a low priority task that requires immediate attention today!")
+            reminder = (task + " is a low priority task that requires immediate attention today!")
+            print(reminder)
         else:
-            print(task, "is a low priority task. Consider completing it when you have free time.")  
+            reminder = (task + " is a low priority task. Consider completing it when you have free time.")
+            print(reminder)
 
     case _:
         print("Invalid Selection")               
