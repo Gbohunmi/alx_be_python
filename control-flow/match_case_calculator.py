b6ceb8b87247cf220512a2e313643277314010d2
Gbohunmi <gbohunmi.odutuga@gmail.com>
2024-12-21 21:51:55 +0100

    Fullstop added to output message

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,22 +1,26 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 operation = str(input("Choose the operation (+, -, *, /):"))
+product = num1 * num2
+addition = num1 + num2
+difference = num1 - num2
+division = num1 / num2
 
 match operation:
     case "/":
         if num2 == 0:
-            print( f"Cannot divide by zero.")
+            print( "Cannot divide by zero.")
         else:
-            print ( f"The result is", num1/num2)
+            print (f"The result is {division}.")
 
     case "+":
-        print ( f"The result is", num1+num2)
+        print (f"The result is {addition}.")
 
     case "-":
-        print ( f"The result is", num1-num2)    
+        print (f"The result is {difference}.")    
          
     case "*":
-        print ( f"The result is", num1*num2)   
+        print (f"The result is {product}.")   
 
     case _:
         print( f'Invalid Operation')      
