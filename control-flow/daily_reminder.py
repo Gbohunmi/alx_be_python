5061e37b57d7be9122f406cc79ed470f795ea31a
Gbohunmi <gbohunmi.odutuga@gmail.com>
2024-12-21 21:40:05 +0100

    Reminder added to output

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -6,26 +6,26 @@ match priority:
     case "high":
         if time_bound == "yes":
             reminder = (task + " is a high priority task that requires immediate attention today!")
-            print(reminder)
+            print('Reminder:',reminder)
         else:
             reminder = (task + " is a high priority task. Consider completing it when you have free time.")
-            print(reminder)
+            print('Note:',reminder)
 
     case "medium":
         if time_bound == "yes":
             reminder = (task + " is a medium priority task that requires immediate attention today!")
-            print(reminder)
+            print('Reminder:',reminder)
         else:
             reminder = (task + " is a medium priority task. Consider completing it when you have free time.")
-            print(reminder)
+            print('Note:',reminder)
 
     case "low":
         if time_bound == "yes":
             reminder = (task + " is a low priority task that requires immediate attention today!")
-            print(reminder)
+            print('Reminder:',reminder)
         else:
             reminder = (task + " is a low priority task. Consider completing it when you have free time.")
-            print(reminder)
+            print('Note:',reminder)
 
     case _:
         print("Invalid Selection")               
