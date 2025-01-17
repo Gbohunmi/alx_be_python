571eaa4ebf4469a7cabcb58f79fd4e78c493a1dc
Gbohunmi <gbohunmi.odutuga@gmail.com>
2025-01-17 14:51:13 +0100

    Removed commas frim init

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,14 +1,12 @@
-
-
 class Book:
-    def __init__(self, title, author,):
+    def __init__(self, title, author):
         self.title = title
         self.author = author
         self.__is_checked_out = False
 
     
 class Library:
-    def __init__(self,):
+    def __init__(self):
         self.__books = []
 
     def add_book(self, book): 
