c464789ba6572223d3cde0a6ba826c4bf13b390b
Gbohunmi <gbohunmi.odutuga@gmail.com>
2025-01-17 15:18:21 +0100

    Edited private attributes and implemented for loop in available books function

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -2,32 +2,44 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
-        self.__is_checked_out = False
+        self._is_checked_out = False
 
     
 class Library:
     def __init__(self):
-        self.__books = []
+        self._books = []
 
     def add_book(self, book): 
-        self.__books.append(book)
+        self._books.append(book)
     
     def check_out_book(self, title):
-        for book in self.__books:
-            if book.title == title and not book.__is_checked_out:
-                book.__is_checked_out = True
+        for book in self._books:
+            if book.title == title and not book._is_checked_out:
+                book._is_checked_out = True
                 return f"Book {title} checked out successfully."
         return f"Book {title} not available."
 
     def return_book(self, title):
-        for book in self.__books:
-            if book.title == title and book.__is_checked_out:
+        for book in self._books:
+            if book.title == title and book._is_checked_out:
                 book.checked_out = False
                 return f"Book {title} returned successfully."
             else:
                 return f"Book {title} not found or not checked out."
 
     def list_available_books(self):
-        available_books = [book for book in self.__books if not book.__is_checked_out]
-        return [book.title for book in available_books]
+        available_books = [book for book in self._books if not book._is_checked_out]
+        if available_books == []:
+            print("No book is availble")
+        else:
+            for book in available_books:
+                print(f"{book.title} by {book.author}")
+    
 
+
+library = Library()
+library.add_book(Book("Brave New World", "Aldous Huxley"))
+library.add_book(Book("1984", "George Orwell"))
+# Initial list of available books
+print("Available books after setup:")
+library.list_available_books()
