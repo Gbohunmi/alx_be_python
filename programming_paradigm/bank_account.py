80afde7c359572be072944d0a5fb39d7bb1ef000
Gbohunmi <gbohunmi.odutuga@gmail.com>
2025-01-17 12:01:30 +0100

    Creating a Simple Bank Account Class

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,23 @@
+class BankAccount:
+    
+    def __init__(self, account_balance = 0.0):
+        self.account_balance = account_balance
+        
+    
+    def deposit(self, amount):
+        self.account_balance += amount
+        return self.account_balance
+             
+    
+    def withdraw(self, amount):
+        self.account_balance = self.account_balance
+        if amount > self.account_balance:
+            self.account_balance = self.account_balance
+            return False
+        else:
+            self.account_balance -= amount
+            return True
+        
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
