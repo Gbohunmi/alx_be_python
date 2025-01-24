f524fba59cb9ab3eb8ae23b5f43acd8992b9c503
Gbohunmi <gbohunmi.odutuga@gmail.com>
2025-01-24 13:24:13 +0100

    Reversed last change

--- oop/library_system.py
+++ oop/library_system.py
@@ -28,13 +28,13 @@ class Library:
     def list_books(self):
          for book in self.books:
             if isinstance(book, PrintBook):
-                return f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}"
+                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
             
             elif isinstance(book, EBook):
-                return f"Ebook: {book.title} by {book.author}, File Size: {book.file_size}KB"
+                print(f"Ebook: {book.title} by {book.author}, File Size: {book.file_size}KB")
             
             else:
-                return f"Book: {book.title} by {book.author}"
+                print(f"Book: {book.title} by {book.author}")
 
 
        
